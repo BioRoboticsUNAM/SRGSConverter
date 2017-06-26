68412b302eb6d5ab7c9a6ade406057973ab80a04
Mauricio Matamoros <kyordhel@gmail.com>
2017-06-26 15:10:41 +0200

    Updated application to work with mono 4.0 and the last version of the CommandGenerator.dll

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using RoboCup.AtHome.CommandGenerator;
 using RoboCup.AtHome.CommandGenerator.Containers;
+using RoboCup.AtHome.CommandGenerator.ReplaceableTypes;
 
 namespace SRGSConverter
 {
@@ -16,10 +17,10 @@ namespace SRGSConverter
 		{
 			List<Grammar> gs = Loader.LoadGrammars();
 			List<Gesture> gestures = Loader.Load<GestureContainer>(Loader.GetPath("Gestures.xml")).Gestures;
-			LocationManager locations = Loader.LoadLocations(Loader.GetPath("Locations.xml"));
+			Loader.LoadLocations(Loader.GetPath("Locations.xml"));
 			List<PersonName> names = Loader.Load<NameContainer>(Loader.GetPath("Names.xml")).Names;
-			List<PredefindedQuestion> questions = Loader.Load<QuestionsContainer>(Loader.GetPath("Questions.xml")).Questions;
-			GPSRObjectManager objects = Loader.LoadObjects(Loader.GetPath("Objects.xml"));
+			List<PredefinedQuestion> questions = Loader.Load<QuestionsContainer>(Loader.GetPath("Questions.xml")).Questions;
+			Loader.LoadObjects(Loader.GetPath("Objects.xml"));
 			if (!Directory.Exists("srgs"))
 				Directory.CreateDirectory("srgs");
 			foreach (Grammar g in gs)
@@ -28,7 +29,7 @@ namespace SRGSConverter
 				{
 					Console.Write("Converting {0}... ", g.Name);
 					string output = Path.Combine("srgs", String.Format("{0}_srgs.xml", g.Name));
-					GrammarConverter.SaveToSRGS(g, output, gestures, locations, names, objects, questions);
+					GrammarConverter.SaveToSRGS(g, output, gestures, names, questions);
 					Console.WriteLine("Saved as {0}", output);
 					Console.WriteLine();
 				}
